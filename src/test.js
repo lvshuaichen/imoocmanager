06d259f61c16c0d7b06cf687a1af569b741f57d3
Lv Shuaichen <85780758@qq.com>
2018-06-26 17:45:39 +0800

    'peizhi'

---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Button } from 'antd'
+
+export default class test extends Component {
+  render () {
+    return (
+      <Button>antd按钮</Button>
+    );
+  }
+}
